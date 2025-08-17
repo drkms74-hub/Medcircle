b4bc1bb216cf213efd9a2501a776a9e97eafa171
drkms74-hub <drkms74@gmail.com>
2025-08-17 12:09:39 +0100

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Welcome to MedCircle 🚑</h1>
+      <p>Your deployment is working!</p>
+    </div>
+  );
+}
