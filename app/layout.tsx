75ef132085a7b6e6ec047f7d72df7fcdbd084849
drkms74-hub <drkms74@gmail.com>
2025-08-17 12:14:30 +0100

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,18 @@
+export const metadata = {
+  title: "MedCircle",
+  description: "Your Acute Medicine Assistant",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+      </body>
+    </html>
+  )
+}
